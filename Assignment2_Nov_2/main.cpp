30eb42dbd3bcc31207d37268ce54aae0f22bd635
Jingnong <wangjingnong@hotmail.com>
2017-11-02 17:19:15 -0700

    Initial Commit

---
+++ Assignment2_Nov_2/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Assignment2_Nov_2
+//
+//  Created by Jingnong Wang on 11/2/17.
+//  Copyright © 2017 Jingnong Wang. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+  // insert code here...
+  std::cout << "Hello, World!\n";
+  return 0;
+}
